bd0d7d0af7642e3cc7d4df5826e8e878c3a9b34e
rafiq9090 <islamrafiq9090@gmail.com>
2026-09-06 17:51:43 +0600

    Add VPS Docker deployment configuration and CI/CD pipeline

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-import "vinext/types/augmentations";
 import "./.next/types/routes.d.ts";
 import "./.next/types/root-params.d.ts";
 
